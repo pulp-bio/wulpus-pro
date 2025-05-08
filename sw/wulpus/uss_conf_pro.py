d808d4d259fd04e15625852d52664e652d982a3e
Cedric Hirschi <cedr02@live.com>
2025-05-08 14:55:24 +0200

    Add envelope detection setting to dongle firmware

--- sw/wulpus/uss_conf_pro.py
+++ sw/wulpus/uss_conf_pro.py
@@ -24,7 +24,7 @@ from wulpus.config_package_pro import *
 START_BYTE_CONF_PACK = 250
 START_BYTE_RESTART   = 251
 # Maximum length of the configuration package
-PACKAGE_LEN  = 72
+PACKAGE_LEN  = 73
 
 # VGA and Digipot Constants
 VGA_RC_SER_RES       = 2.7e3
